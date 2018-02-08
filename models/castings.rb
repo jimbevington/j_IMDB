b2e19e1f74f1d07856b87c19225f287d235a5cc2
Jim Bevington <jim.bevington@hotmail.com>
2018-02-08 15:42:36 +0000

    added calc_budget_deduction and update_film_budget_available to castings. budget_available for movies not initializing correctly

--- models/castings.rb
+++ models/castings.rb
@@ -1,4 +1,6 @@
 require_relative('../db/sql_runner.rb')
+require_relative('../models/movies.rb')
+
 
 class Casting
 
@@ -12,19 +14,37 @@ class Casting
     @fee = options['fee']
   end
 
-  # def update_movie_budgets
-  #   # calc deduction
-  #   sql = "SELECT castings.fee FROM castings INNER JOIN moviestars
-  #          ON castings.moviestar_id = moviestars.id
-  #          WHERE castings.movie_id = $1"
-  #   values = [@movie_id]
-  # end
+  def calc_budget_deduction
+    # calc deduction
+    sql = "SELECT castings.fee FROM castings INNER JOIN moviestars
+           ON castings.moviestar_id = moviestars.id
+           WHERE castings.movie_id = $1"
+    values = [@movie_id]
+    result = SqlRunner.run(sql, values)
+    fee_array = result.map{|movie| movie['fee'].to_i}
+    deduction = if fee_array.length > 0
+                  fee_array.sum
+                else
+                  0
+                end
+    return deduction
+  end
+
+  def update_film_budget_available
+    deduction = calc_budget_deduction()
+    movie_budget = Movie.find_by_id(@movie_id).budget
+    new_sum = movie_budget.to_i - deduction.to_i
+    sql = "UPDATE movies SET budget_available = $1 WHERE id = $2"
+    values = [new_sum, @movie_id]
+    SqlRunner.run(sql, values)
+  end
 
   def save()
     sql = "INSERT INTO castings (movie_id, moviestar_id, fee)
            VALUES ($1, $2, $3) RETURNING id"
     values = [@movie_id, @moviestar_id, @fee]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
+    update_film_budget_available()
   end
 
   def delete()
